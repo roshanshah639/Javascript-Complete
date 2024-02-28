86aee9bd3a85b17460201baaf3d745ec311a6b10
roshan shah <roshanshah639@gmail.com>
2024-02-28 11:14:02 +0545

    For Loops

---
+++ 05_Loops/01_iterations.js
@@ -0,0 +1,39 @@
+// for loop
+for (let i = 0; i <= 10; i++) {
+  const number = i;
+  if (number === 5) {
+    // console.log("5 is Best Number");
+  }
+  // console.log(number);
+}
+
+for (let i = 1; i <= 10; i++) {
+  // console.log(`Table of: ${i}`);
+  for (let j = 1; j <= 10; j++) {
+    // console.log(`Inner Loop: ${j} and Outer Loop: ${i}`);
+    // console.log(i * j);
+  }
+}
+
+const myArray = ["Flash", "Thor", "Spiderman", "Hulk"];
+for (let i = 0; i < myArray.length; i++) {
+  const hero = myArray[i];
+  // console.log(hero);
+}
+
+// break, continue
+// for (let i = 1; i <= 20; i++) {
+//   if (i === 5) {
+//     console.log("5 is detected.");
+//     break;
+//   }
+//   console.log(`The value of i is: ${i}`);
+// }
+// for (let i = 1; i <= 20; i++) {
+//   if (i === 5) {
+//     console.log("5 is detected.");
+//     continue;
+//   }
+//   console.log(`The value of i is: ${i}`);
+// }
+
